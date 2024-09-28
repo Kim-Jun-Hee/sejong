7408ebbaafd3a0025b622957f5cbc993bc4e3c09
Kim-Jun-Hee <juni991212@gmail.com>
2024-09-29 00:50:46 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -15,7 +15,7 @@ import re
 
 
 # 데이터 불러오기 cp949
-df = pd.read_csv('https://raw.githubusercontent.com/Kim-Jun-Hee/sejong/main/음식점정보홈페이지활용_메뉴최종.csv', encoding='euc-kr')
+df = pd.read_csv('https://raw.githubusercontent.com/Kim-Jun-Hee/sejong/main/음식점정보홈페이지활용_메뉴최종.csv', encoding='latin1')
 age_rankings_df = pd.read_csv("https://raw.githubusercontent.com/Kim-Jun-Hee/sejong/main/나이대성별선호음식순위.csv",encoding='utf-8')
 
 
